f10f7b08062b67a585fc833885edd2e307e9d13c
maiyume <kietpg@vng.com.vn>
2023-11-15 02:03:55 +0700

    modified:   css/style.css 	modified:   index.htm 	modified:   script/script.js

--- script/script.js
+++ script/script.js
@@ -12,6 +12,6 @@ function calculateResult() {
     try {
         display.value = eval(display.value);
     } catch (error) {
-        display.value = 'Error';
+        display.value = 'You dum dum';
     }
 }
